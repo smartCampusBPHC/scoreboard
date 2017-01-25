464e575b1ef93f6ec961b971200ecacb358732ed
ayush7 <ayushyadav173@gmail.com>
2017-01-25 11:45:47 +0530

    Score_updating page made; Now would work on get and post request; then move to monitor display page

--- runscoreserver.py
+++ runscoreserver.py
@@ -5,19 +5,38 @@ import datetime
 import csv
 import requests
 from collections import deque
-
 from tempfile import NamedTemporaryFile
 import shutil
-
 from flask import Flask,request ,render_template
+from numpy import genfromtxt
 
 app=Flask(__name__)
+
+
+#live_scores = genfromtxt('my_file.csv', delimiter=',')
+
+dict = {'Sports':'-1' ,'MatchNumber':'-1','Gender':'M','Team1':'BPHC','Team2':'-1','Score':'-1','Status':'-1'}
+# def edit_live(Sports,MatchNumber,Gender,Team1,Team2,Score,Status):
+#     with open('diopter.csv','rb') as fread:
+#         with open('diopter.csv','wb') as fwrite:
+#             reader = csv.reader(fread) #open csv
+
+            # next(reader,None)
+            # for row in reader:
+            #     if row[1] == Sports:
+
+
+#Supposed to give out a list of all 
+# def edit_complete_list(Sports,MatchNumber,Gender,Team1,Team2,Score,Status):
+    
+#     pass
+
 
 
 
 @app.route('/', methods=['GET', 'POST'])
 def front():
-    return render_template('screen_web.html')
+    return render_template('score_updater.html')
 
 
 @app.route('/tvscreen', methods=['GET', 'POST'])
@@ -26,26 +45,7 @@ def screen_tv_show():
 
 @app.route('/updater', methods=['GET','POST'])
 def main_updater():
-    
-    pass
-
-
-def edit_live(Sports,MatchNumber,Gender,Team1,Team2,Score,Status):
-    with open('diopter.csv','rb') as fread:
-        with open('diopter.csv','wb') as fwrite:
-            reader = csv.reader(fread) #open csv
-
-            next(reader,None)
-            for row in reader:
-                if row[1] == Sports:
-
-
-    pass
-
-def edit_complete_list(Sports,MatchNumber,Gender,Team1,Team2,Score,Status):
-    
-    pass
-
+    return 0
 
 # @app.route('/login', methods=['GET', 'POST'])
 # def login():
